e7e40c2f048fb59e0eefb6e7152dadb20619c9cf
YousifAlY <yousifucv@hotmail.com>
2020-01-31 23:50:26 -0500

    Added more play car code.

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   key: 'Game',
 };
 
-var controls;
 export class GameScene extends Phaser.Scene {
   private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
   private terrain: Terrain = new Terrain();
@@ -28,24 +27,34 @@ export class GameScene extends Phaser.Scene {
     var worldWidth = 600;
     var worldHeight = 600;
     this.matter.world.setBounds(0, 0, worldWidth, worldHeight);
-    this.matter.add.rectangle(300,300, 100, 20, { chamfer:{radius: 10}, restitution: 0.7 });
 
-    this.matter.add.circle(300,200, 20, {restitution: 0.7 });
-    this.matter.add.circle(300,100, 20, {restitution: 0.7 });
     this.matter.add.mouseSpring();
 
 
     // add stiff multi-body constraint
-    var bodyA = this.matter.bodies.polygon(100, 400, 60, 20);
-    var bodyB = this.matter.bodies.polygon(200, 400, 1, 50);
+    var carBody = this.matter.bodies.rectangle(300, 100, 100, 20, { chamfer:{radius: 10}, restitution: 0.7 });
+    var wheelA = this.matter.bodies.circle(100, 300, 20);
+    var wheelB = this.matter.bodies.circle(200, 300, 20);
 
-    var constraint = this.matter.constraint.create({
-        bodyA: bodyA,
+    var car = this.matter.composites.car(300,300,300,10,20);
+    this.matter.world.add(car);
+
+    var constraint: MatterJS.ConstraintType;
+    constraint = this.matter.constraint.create({
+        bodyA: wheelA,
         pointA: { x: 0, y: 0 },
-        bodyB: bodyB,
-        pointB: { x: 0, y: 0 }
+        bodyB: carBody,
+        pointB: { x: 10, y: 0 }
     });
-    this.matter.world.add([bodyA, bodyB, constraint]);
+    this.matter.world.add([wheelA, carBody, wheelB]);
+
+    // constraint = this.matter.constraint.create({
+    //     bodyA: wheelB,
+    //     pointA: { x: 0, y: 0 },
+    //     bodyB: carBody,
+    //     pointB: { x: -10, y: 0 }
+    // });
+    // this.matter.world.add([wheelB, carBody, constraint]);
 
     
     this.terrain.create(this);

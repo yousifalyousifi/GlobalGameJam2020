940f69888fd68a336f31b69925d355aaadf308fe
Ming <git@user00.com>
2020-01-31 23:35:55 -0500

    Tilemap working with matter physics

--- src/terrain.ts
+++ src/terrain.ts
@@ -8,6 +8,8 @@ export class Terrain
 
   }
   create(scene: Phaser.Scene) {
+    const yOffset = 500;
+
     // Make some rough sample tiles
     let level: number[][] = [];
     for (let row = 0; row < 50; row++) {
@@ -18,15 +20,22 @@ export class Terrain
             line.push(0);
         else
             line.push(1);
-    }
+      }
     }
 
     // Create a tilemap and tileset
     const map = scene.make.tilemap({data: level, tileWidth: 2, tileHeight: 2});
     const tiles = map.addTilesetImage("ground-tiles");
-    const layer = map.createStaticLayer(0, tiles, 0, 100);
+    const layer = map.createStaticLayer(0, tiles, 0, yOffset);
 
-    // Tile 0 is colliding
-    layer.setCollisionBetween(0, 0);
+    // Create physics bodies for the tiles
+    let staticGroup = scene.matter.add
+    for (let row = 0; row < 50; row++) {
+        for (let col = 0; col < 100; col++) {
+          if (level[row][col] == 1) continue;
+          let rect = scene.matter.add.rectangle(col * 2, row * 2 + yOffset, 2, 2, { chamfer:{radius: 0.5}, restitution: 0.5 });
+          rect.isStatic = true;
+        }
+    }
   }
 }
